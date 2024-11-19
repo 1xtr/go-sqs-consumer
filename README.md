a1c2b1beeceaaf1cfb5fd1330b2128cefafe3468
Alex <i@1xtr.dev>
2024-11-20 00:20:54 +0300

    feat: added consumer

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# go-sqs-consumer
-Consumer for AWS SQS on Go
+# SQS consumer on Golang
+> Use AWS SDK for Go V2 and zerolog
+
+## Description
+
+Very simple consumer for proceed messages from queue there we can set:
+ - batchSize (Max 10, default 1)
+ - waitTimeSeconds (Default is 20s)
+ - MessageAttributeNames (Default [])
+ - messageSystemAttributeNames (Default [])
+ - pollDelayInMs (default 0)
+ - visibilityTimeout (default 0)
+ - shouldDeleteMessages flag for cases there don't need to delete the message (Default true - delete message)
+
+Default log level is `info`
+## Usage example
+
+```go
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	consumer "github.com/1xtr/go-sqs-consumer"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+	log := consumer.GetLogger("main")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Error().Err(err).Msgf("error load sdk config: %v", err)
+		return
+	}
+	opts := consumer.Options{
+		QueueUrl:              "https://sqs.eu-central-1.amazonaws.com/1234567890/development",
+		SqsClient:             sqs.NewFromConfig(cfg),
+		HandleMessage:         handler,
+		ShouldDeleteMessages:  aws.TrueTernary,
+		WaitTimeSeconds:       1,
+		MessageAttributeNames: []string{"All"},
+		MessageSystemAttributeNames: []types.MessageSystemAttributeName{
+			types.MessageSystemAttributeNameAll,
+		},
+	}
+	c := consumer.New(opts)
+	c.Start()
+
+	// Graceful shutdown
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigCh
+	log.Debug().Msg("Gracefully shutting down...")
+	log.Debug().Msg("Running cleanup tasks...")
+
+	c.Stop()
+	log.Debug().Msg("App was successful shutdown.")
+}
+
+func handler(ctx context.Context, msg *types.Message) error {
+	log := zerolog.Ctx(ctx).With().Str("MessageId", *msg.MessageId).
+		Str("component", "handler").Logger()
+
+	log.Info().Interface("message", msg).Msgf("message received")
+	time.Sleep(5 * time.Second)
+	if strings.Contains(*msg.Body, "error") {
+		return fmt.Errorf("an error occured")
+	}
+	return nil
+}
+```
+---
+
+### Tests
+
+For local tests we can set environment variable `LOG_TO_FILE=true` for save execution log to `./logs` folder.
---
+++ consumer.go
@@ -0,0 +1,212 @@
+package consumer
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/rs/zerolog"
+)
+
+type (
+	Consumer struct {
+		sqsClient                   *sqs.Client
+		queueUrl                    string
+		handler                     func(c context.Context, m *types.Message) error
+		stopSignal                  chan os.Signal
+		messagesChannel             chan types.Message
+		batchSize                   int
+		pollDelayInMs               int
+		visibilityTimeout           int
+		waitTimeSeconds             int
+		MessageAttributeNames       []string
+		messageSystemAttributeNames []types.MessageSystemAttributeName
+		shouldDeleteMessages        bool
+		logger                      zerolog.Logger
+	}
+	Options struct {
+		QueueUrl                    string
+		SqsClient                   *sqs.Client
+		BatchSize                   int
+		pollDelayInMs               int
+		VisibilityTimeout           int
+		WaitTimeSeconds             int
+		MessageAttributeNames       []string
+		MessageSystemAttributeNames []types.MessageSystemAttributeName
+		HandleMessage               func(c context.Context, m *types.Message) error
+		ShouldDeleteMessages        aws.Ternary
+	}
+)
+
+var (
+	defaultRegion = "eu-central-1"
+	region        = os.Getenv("AWS_REGION")
+)
+
+func New(o Options) *Consumer {
+	c := Consumer{
+		queueUrl:                    o.QueueUrl,
+		sqsClient:                   o.SqsClient,
+		handler:                     o.HandleMessage,
+		stopSignal:                  make(chan os.Signal, 1),
+		messagesChannel:             make(chan types.Message),
+		batchSize:                   o.BatchSize,
+		pollDelayInMs:               o.pollDelayInMs,
+		visibilityTimeout:           o.VisibilityTimeout,
+		waitTimeSeconds:             o.WaitTimeSeconds,
+		MessageAttributeNames:       o.MessageAttributeNames,
+		messageSystemAttributeNames: o.MessageSystemAttributeNames,
+		shouldDeleteMessages:        true,
+		logger:                      GetLogger("Consumer"),
+	}
+	// If SQS Client not set, use default
+	if c.sqsClient == nil {
+		if region == "" {
+			region = defaultRegion
+		}
+		c.sqsClient = sqs.New(
+			sqs.Options{
+				Region:           region,
+				RetryMaxAttempts: 3,
+				RetryMode:        "adaptive",
+			},
+		)
+	}
+
+	// If batch size not set use default 1
+	if c.batchSize == 0 {
+		c.batchSize = 1
+	}
+	// AWS batch size limit of 10,
+	// https//docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/quotas-messages.html
+	if c.batchSize > 10 {
+		c.batchSize = 10
+	}
+
+	if c.waitTimeSeconds == 0 {
+		c.waitTimeSeconds = 20
+	}
+
+	if o.ShouldDeleteMessages != aws.UnknownTernary {
+		c.shouldDeleteMessages = o.ShouldDeleteMessages.Bool()
+	}
+
+	return &c
+}
+
+func (c *Consumer) Start() {
+	log := GetLogger("Start")
+
+	log.Debug().
+		Str("queue", c.queueUrl).
+		Msgf("consumer starting")
+
+	signal.Notify(c.stopSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start message processing in a single goroutine
+	go c.processMessages()
+
+	// Poll messages in a loop
+	c.pollMessages()
+
+	// Wait for stop signal
+	<-c.stopSignal
+	c.logger.Debug().Msgf("Shutdown signal received. Stopping...")
+	close(c.messagesChannel)
+}
+
+func (c *Consumer) wait() {
+	time.Sleep(time.Millisecond * time.Duration(c.pollDelayInMs))
+}
+
+// waitForProcessing ensures that all messages in the current batch are processed before fetching new messages.
+func (c *Consumer) waitForProcessing() {
+	// Wait for the message channel to drain
+	for len(c.messagesChannel) > 0 {
+		time.Sleep(100 * time.Millisecond) // Small delay to check channel status
+	}
+}
+
+func (c *Consumer) pollMessages() {
+	log := GetLogger("pollMessages")
+	for {
+		select {
+		case <-c.stopSignal:
+			log.Debug().Msgf("stop signal received, shutting down message receiver")
+			close(c.messagesChannel)
+			return
+		default:
+			result, err := c.sqsClient.ReceiveMessage(
+				context.TODO(),
+				&sqs.ReceiveMessageInput{
+					MessageSystemAttributeNames: c.messageSystemAttributeNames,
+					MessageAttributeNames:       c.MessageAttributeNames,
+					QueueUrl:                    aws.String(c.queueUrl),
+					MaxNumberOfMessages:         int32(c.batchSize),
+					VisibilityTimeout:           int32(c.visibilityTimeout),
+					WaitTimeSeconds:             int32(c.waitTimeSeconds),
+				},
+			)
+
+			if err != nil {
+				log.Error().Err(err).Caller().Msgf("error receive messages: %v", err)
+				return
+			}
+			log.Debug().Interface("result", result).Msgf("pollMessages.result")
+			if len(result.Messages) > 0 {
+				for _, message := range result.Messages {
+					c.messagesChannel <- message
+				}
+			}
+
+			c.waitForProcessing()
+		}
+	}
+}
+
+func (c *Consumer) processMessages() {
+	ctx := Logger.WithContext(context.Background())
+	log := GetLogger("processMessages")
+	for msg := range c.messagesChannel {
+		err := c.handler(ctx, &msg)
+		if err != nil {
+			log.Error().Err(err).
+				Interface("message", msg).
+				Msgf("Error processing message: %v\n", err)
+			continue
+		}
+
+		// Delete the message from SQS after successful processing
+		if c.shouldDeleteMessages {
+			go c.deleteMessage(ctx, &msg)
+		}
+	}
+}
+
+// Stop gracefully shuts down the consumer.
+func (c *Consumer) Stop() {
+	close(c.stopSignal)
+}
+
+func (c *Consumer) deleteMessage(ctx context.Context, msg *types.Message) {
+	log := zerolog.Ctx(ctx).With().Str("MessageId", *msg.MessageId).
+		Str("component", "deleteMessage").Logger()
+	// Delete the message from SQS after successful processing
+	if c.shouldDeleteMessages {
+		log.Debug().Msgf("deleting message %s", *msg.MessageId)
+		_, err := c.sqsClient.DeleteMessage(
+			context.Background(), &sqs.DeleteMessageInput{
+				QueueUrl:      &c.queueUrl,
+				ReceiptHandle: msg.ReceiptHandle,
+			},
+		)
+		if err != nil {
+			log.Error().Err(err).Msgf("error deleting message %v", err)
+		}
+	}
+}
